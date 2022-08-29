d3321658959df29768014b0756d73fd99b243249
ZY Liu <1432416264@qq.com>
2022-08-29 13:08:32 +0800

    frontdev

--- back_dev/src/main/java/com/example/back_dev/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.back_dev.dao;
-
-import com.example.back_dev.bean.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserDao {
-    public User getUserById(int id);
-}
