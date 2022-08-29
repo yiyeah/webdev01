033de60fa7f0c9b2bb5153a2b9f179d9b0a1729b
ZY Liu <1432416264@qq.com>
2022-08-29 12:29:43 +0800

    Update HelloWorld.vue
    test

--- front_dev/test_front/src/components/HelloWorld.vue
+++ front_dev/test_front/src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
     <input v-model="form.id">
     <span>search</span>
     <button @click="clickbutton()">{{ click }}</button>
-    <span>test</span>
+    <span>test1</span>
   </div>
 </template>
 
