2f5dcffa1b8a397e4f9a371c88511991a8f717f3
ZY Liu <1432416264@qq.com>
2022-08-29 17:27:33 +0800

    test

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import store from './store'
 
 import axios from 'axios'
 Vue.prototype.$http = axios
-// axios.defaults.baseURL = "http://43.140.248.122:8090"
-axios.defaults.baseURL = "http://localhost:8090"
+axios.defaults.baseURL = "http://43.140.248.122:8090"
+// axios.defaults.baseURL = "http://localhost:8090"
 
 Vue.config.productionTip = false
 
